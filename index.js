a8dbd950554dac7aee5ab7fcc420c744ec9c2250
Katie Greer <katieggreer@gmail.com>
2024-02-05 09:43:29 -0700

    random css stuff still not as responsive as I want

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 function login() { //TODO: must have correct username and password
     const nameEl = document.querySelector("#exampleUsername");
-    const passwordEl = document.querySelector("#examplePassword");
     localStorage.setItem("userName", nameEl.value);
-    localStorage.setItem("passWord", nameEl.value);
     window.location.href = "all_recipes.html";
   }
 //Login: username and password must be in the database
