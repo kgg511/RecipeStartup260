151dc4219f76999abbb8fcea2e2b5422cbd938a9
Katie Greer <katieggreer@gmail.com>
2024-02-06 21:11:36 -0700

    all_recipes now displays the recipes of all users

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
 
-function login() { //TODO: must have correct username and password
+async function login() { //TODO: must have correct username and password
     const nameEl = document.querySelector("#exampleUsername");
     localStorage.setItem("UserName", nameEl.value);
+
+    await update_usernames(nameEl.value);
+
     console.log(`your username is ${nameEl.value}`);
     window.location.href = "all_recipes.html";
     
   }
 //Login: username and password must be in the database
-
-
-
 //Register: they can't use a username that is already in use
+
+async function update_usernames(username){
+  const usernames = JSON.parse(localStorage.getItem("Usernames")) || [];
+  usernames.push(username);
+  localStorage.setItem("Usernames", JSON.stringify(usernames));
+}
+
+//localStorage.setItem(`recipes_${username}`, JSON.stringify(RecipesDict));
