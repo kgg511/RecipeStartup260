36e260649a5e9f2c209ffaea690dedf447147bc1
Katie Greer <katieggreer@gmail.com>
2024-02-29 14:36:42 -0700

    3rd party call works

--- public/index.js
+++ public/index.js
@@ -20,39 +20,23 @@ async function update_usernames(username){
     localStorage.setItem("Usernames", JSON.stringify(usernames));
   }
 }
-//const axios = require('axios');
 
-
-async function displayImaged() {
-  fetch('https://foodish-api.com/images/samosa/samosa20.jpg')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("i got the data");
-      const containerEl = document.querySelector('#image');
-      image_url = data['image']
-      containerEl.src = image_url
-    });
-}
 //{"image":"https://foodish-api.com/images/burger/burger101.jpg"}
-//https://foodish-api.herokuapp.com/api/'
-
 //this works 'https://foodish-api.com/images/samosa/samosa20.jpg'
 function displayImage() {
-  fetch('https://foodish-api.com')
-  //src="/images/samosa/samosa21.jpg"
+  fetch('https://foodish-api.com/api/')
     .then((response) => {
       // Check if the response is OK
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      return response.blob(); // Get the response as a Blob
+      return response.json();
     })
-    .then((blob) => {
-      // Convert the Blob to a URL
-      const imageUrl = URL.createObjectURL(blob);
-      console.log(`the image url is ${imageURL}`);
+    .then((data) => {
+      console.log(data);
+      const imageUrl = data["image"];
+      console.log(`the image url is ${imageUrl}`);
       
-      // Set the image URL to the container's src attribute
       const containerEl = document.querySelector('#image');
       containerEl.src = imageUrl;
     })
@@ -61,23 +45,7 @@ function displayImage() {
     });
 }
 
-function displayPicture() {
-  const random = Math.floor(Math.random() * 1000);
-  fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("i got the data");
-      const containerEl = document.querySelector('#image');
-      //image_url = data['image']
-      //containerEl.src = data.content
-      //containerEl.src = image_url
-      const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
-      //const imgEl = document.createElement('img');
-      containerEl.src = imgUrl
-      //imgEl.setAttribute('src', imgUrl);
-      //containerEl.appendChild(imgEl);
-    });
-}
+
 
 //displayPicture();
-//displayImage();
+displayImage();
