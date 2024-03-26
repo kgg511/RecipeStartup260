95c70688c800b004e61aaeb2f4557e8df61a146f
Katie Greer <katieggreer@gmail.com>
2024-03-26 16:33:22 -0600

    added header

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+      <div className='body'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              TasteTrove <i class="fa-solid fa-utensils"></i>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link' href='index.html'>
+                  My Recipes
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='play.html'>
+                  All Recipes
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='scores.html'>
+                  Add Recipe
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='about.html'>
+                  About
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <main>App components go here</main>
+  
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
