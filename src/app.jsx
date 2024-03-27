be227f65fda81c0a8b70e8a976b3d1a1f980fc99
Katie Greer <katieggreer@gmail.com>
2024-03-26 21:40:24 -0600

    header and footer added

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import {Login} from './login/login';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 export default function App() {
     return (
       <div className='body'>
-        <header className='container-fluid'>
+        <header>
+            <nav>
+                <ul><h1>TasteTrove <i class="fa-solid fa-utensils"></i></h1> </ul>
+                <ul><h5><button class = "headerLink" id="SignOut" onclick="signOut()">Sign out</button></h5></ul> 
+                <ul><h5><button class = "headerLink" onclick = "window.location.href='all_recipes.html'">All Recipes</button></h5></ul>  
+                <ul><h5><button class = "headerLink" onclick = "window.location.href='add_recipe.html'">Add Recipe</button></h5></ul>     
+            </nav>
+        </header>
+
+        
+        {/* <header className='container-fluid'>
           <nav className='navbar fixed-top navbar-dark'>
             <div className='navbar-brand'>
               TasteTrove <i class="fa-solid fa-utensils"></i>
@@ -26,25 +38,18 @@ export default function App() {
                   Add Recipe
                 </a>
               </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='about.html'>
-                  About
-                </a>
-              </li>
             </menu>
           </nav>
-        </header>
+        </header> */}
   
         <main>App components go here</main>
   
-        <footer className='bg-dark text-white-50'>
-          <div className='container-fluid'>
-            <span className='text-reset'>Author Name(s)</span>
-            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
-              Source
-            </a>
-          </div>
+        <footer>
+            <hr />
+            <span class="text-reset">Katherine Greer</span>
+            <a href="https://github.com/kgg511/RecipeStartup260">GitHub</a>
         </footer>
+        
       </div>
     );
   }
